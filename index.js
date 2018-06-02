8fa9aef3964e46c2fdf0833dd06f4ef35a796dbf
Niv Sardi <xaiki@evilgiggle.com>
2018-06-02 10:33:52 -0300

    es6 classes
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- index.js
+++ index.js
@@ -1,102 +1,100 @@
-var inherits = require('util').inherits
-  , torrentStream = require('torrent-stream')
-  , readTorrent = require('read-torrent')
-  , crypto = require('crypto');
+const torrentStream = require('torrent-stream')
+const readTorrent = require('read-torrent')
+const crypto = require('crypto')
 
-var Streamer = require('butter-base-streamer');
+const Streamer = require('butter-base-streamer')
+const config = {
+  name: 'Torrents and Magnet Links Streamer',
+  suffix: /(torrent)/,
+  protocol: /(torrent|magnet)/,
+  type: 'torrent',
+  priority: 50
+}
 
 /* -- Torrent Streamer -- */
-function TorrentStreamer(source, options) {
-	if(!(this instanceof TorrentStreamer))
-		return new TorrentStreamer(source, options);
+class TorrentStreamer extends Streamer {
+  constructor (source, options) {
+    super(options)
+    this.config = config
+    options = options || {}
 
-	Streamer.call(this, options);
-	var self = this;
-	options = options || {};
+    if (options.torrent &&
+        options.torrent.id &&
+        options.torrent.id.length < 20) {
+      var idRemainder = 20 - options.torrent.id.length
+      var remainderHash = crypto.createHash('sha1')
+        .update(crypto.pseudoRandomBytes(idRemainder))
+        .digest('hex')
+        .slice(0, idRemainder)
+      options.torrent.id += remainderHash
+    }
 
-	if(options.torrent &&
-	   options.torrent.id &&
-	   options.torrent.id.length < 20) {
-		var idRemainder = 20 - options.torrent.id.length;
-		var remainderHash = crypto.createHash('sha1')
-							.update(crypto.pseudoRandomBytes(idRemainder))
-							.digest('hex')
-							.slice(0, idRemainder);
-		options.torrent.id += remainderHash;
-	}
+    this._ready = false
 
-	this._ready = false;
+    readTorrent(source, (err, torrent) => {
+      if (err) throw err
 
-	readTorrent(source, function(err, torrent) {
-		if(err) throw err;
+      this._torrentStream = torrentStream(torrent, options.torrent)
+      this._torrentStream.on('uninterested', () => (this._torrentStream.swarm.pause()))
+      this._torrentStream.on('interested', () => (this._torrentStream.swarm.resume()))
 
-		self._torrentStream = torrentStream(torrent, options.torrent);
-		self._torrentStream.on('uninterested', function() { self._torrentStream.swarm.pause() });
-		self._torrentStream.on('interested',   function() { self._torrentStream.swarm.resume() });
+      this._torrentStream.on('ready', () => {
+        if (typeof options.fileIndex !== 'number') {
+          var index = this._torrentStream.files.reduce((a, b) => (
+            a.length > b.length ? a : b
+          )
+          )
+          index = this._torrentStream.files.indexOf(index)
+        }
 
-		self._torrentStream.on('ready', function() {
-			if (typeof options.fileIndex !== 'number') {
-				var index = self._torrentStream.files.reduce(function(a, b) {
-					return a.length > b.length ? a : b;
-				});
-				index = self._torrentStream.files.indexOf(index);
-			}
+        this._torrentStream.files[index].select()
+        this.filesize = this._torrentStream.torrent.files[index].length
+        this._progress.setLength(this.file.length)
+        this._streamify.resolve(this.file.createReadStream())
+        this._isReady(this._torrentStream.files[index])
+      })
+    })
+  }
+  _requestProgress () {
+    var swarm = this._torrentStream.swarm
+    return {
+      pieces: swarm.piecesGot,
+      size: this.filesize,
+      peers: swarm.wires.filter(wire => (!wire.peerChoking && wire.peerInterested)).length,
+      seeds: swarm.wires.filter(wire => (!wire.peerInterested)).length,
+      connections: swarm.wires.length,
+      uploadSpeed: swarm.uploadSpeed()
+    }
+  }
 
-			self._torrentStream.files[index].select();
-			self.filesize = self._torrentStream.torrent.files[index].length;
-			self._progress.setLength(self.file.length);
-			self._streamify.resolve(self.file.createReadStream());
-			self._isReady(self._torrentStream.files[index]);
-		})
-	})
-}
-inherits(TorrentStreamer, Streamer);
+  seek (start, end) {
+    if (this._destroyed) throw new ReferenceError('Streamer already destroyed')
+    if (!this._ready) return
 
-TorrentStreamer.prototype.config = {
-	name: 'Torrents and Magnet Links Streamer',
-	suffix: /(torrent)/,
-	protocol: /(torrent|magnet)/,
-	type: 'torrent',
-	priority: 50
-}
+    var opts = {
+      start: start
+    }
 
-TorrentStreamer.prototype._requestProgress = function() {
-	var swarm = this._torrentStream.swarm;
-	return {
-		pieces: swarm.piecesGot,
-		size: this.filesize,
-		peers: swarm.wires.filter(function(wire) {return !wire.peerChoking && wire.peerInterested}).length,
-		seeds: swarm.wires.filter(function(wire) {return !wire.peerInterested}).length,
-		connections: swarm.wires.length,
-		uploadSpeed: swarm.uploadSpeed()
-	};
-}
+    if (end !== undefined) {
+      opts.end = end
+    }
 
-TorrentStreamer.prototype.seek = function(start, end) {
-	if(this._destroyed) throw new ReferenceError('Streamer already destroyed');
-	if(!this._ready) return;
+    this._streamify.unresolve()
+    this._streamify.resolve(this.file.createReadStream(opts))
+  }
 
-	var opts = {
-		start: start
-	}
+  destroy () {
+    if (this._destroyed) throw new ReferenceError('Streamer already destroyed')
 
-	if(end !== undefined) {
-		opts.end = end;
-	}
-
-	this._streamify.unresolve();
-	this._streamify.resolve(this.file.createReadStream(opts));
-}
-
-TorrentStreamer.prototype.destroy = function() {
-	if(this._destroyed) throw new ReferenceError('Streamer already destroyed');
-
-	this._torrentStream.destroy();
-	this._streamify.unresolve();
-	this._ready = false;
-	this._torrentStream = null;
-	this.file = {};
-	this._destroyed = true;
+    this._torrentStream.destroy()
+    this._streamify.unresolve()
+    this._ready = false
+    this._torrentStream = null
+    this.file = {}
+    this._destroyed = true
+  }
 }
 
-module.exports = TorrentStreamer;
+TorrentStreamer.config = config
+
+module.exports = TorrentStreamer
